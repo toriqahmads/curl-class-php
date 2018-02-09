e5881deb6b68ee877788e7dc97163641fe096159
Toriq Ahmad <33996579+toriqahmads@users.noreply.github.com>
2018-02-09 17:45:34 +0700

    Update curl.class.php

--- curl.class.php
+++ curl.class.php
@@ -4,12 +4,18 @@ class curl
 {
 	private $option = array();
 	private $headers = array();
+	private $settings = array();
 	private $curl;
 	private $curlopt;
 
 	public function __construct()
 	{
 		$this->curl = curl_init();
+	}
+
+	public function settings($settings)
+	{
+		$this->settings = $settings;
 	}
 
 	public function setUrl($url)
@@ -95,6 +101,15 @@ class curl
 		$this->setOpt(CURLOPT_POST, true);
 	}
 
+	public function setCustomReq($req, $data = array())
+	{
+		$this->setOpt(CURLOPT_CUSTOMREQUEST, $req);
+		if(isset($data))
+		{
+			$this->setOpt(CURLOPT_POSTFIELDS, $data);
+		}
+	}
+
 	public function setHeaderOneHit($headers)
 	{
 		$this->setOpt(CURLOPT_HTTPHEADER, $headers);
@@ -118,6 +133,11 @@ class curl
 	public function setHeaderEncode($encode)
 	{
 		$this->headers['Accept-Encoding'] = $encode;
+	}
+
+	public function setHeaderExtraHeader($headername, $headervalue)
+	{
+		$this->headers[$headername] = $headervalue;
 	}
 
 	public function setHeaderHost($host)
@@ -210,9 +230,167 @@ class curl
 
 	public function buildJsonPost($data = array())
 	{
-		$data = json_encode($data)
+		$data = json_encode($data);
 
 		return $data;
+	}
+
+	public function curl()
+	{
+		if(isset($this->settings['url']))
+		{
+			$this->setUrl($this->settings['url']);
+		}
+		else
+		{
+			throw new Exception("The URL paramater couldn't be empty");						
+		}
+
+		if(isset($this->settings['url_ref']))
+		{
+			$this->setRef($this->settings['url_ref']);
+		}
+
+		if(isset($this->settings['useragent']))
+		{
+			$this->setUserAgent($this->settings['useragent']);
+		}
+
+		if(isset($this->settings['follow_location']))
+		{
+			$this->setFollow($this->settings['follow_location']);
+		}
+
+		if(isset($this->settings['return_transfer']))
+		{
+			$this->setTransfer($this->settings['return_transfer']);
+		}
+
+		if(isset($this->settings['header']))
+		{
+			$this->setHeader($this->settings['header']);
+		}
+
+		if(isset($this->settings['cookie_string']))
+		{
+			$this->setCookie($settings['cookie_string']);
+		}
+
+		if(isset($this->settings['cookie_jar']))
+		{
+			$this->setCookieJar($this->settings['cookie_jar']);
+		}
+
+		if(isset($this->settings['cookie_file']))
+		{
+			$this->setCookieFile($this->settings['cookie_file']);
+		}
+
+		if(isset($this->settings['socks5']))
+		{
+			$this->setSocks5($this->settings['socks5']);
+		}
+
+		if(isset($this->settings['http_headers']))
+		{
+			$this->setHeaderOneHit($this->settings['http_header']);
+		}
+
+		if(isset($this->settings['header_builder']))
+		{
+			if(isset($this->settings['header_builder']['host']))
+			{
+				$this->setHeaderHost($this->settings['header_builder']['host']);
+			}
+
+			if(isset($this->settings['header_builder']['accept']))
+			{
+				$this->setHeaderAccept($this->settings['header_builder']['accept']);
+			}
+
+			if(isset($this->settings['header_builder']['accept_lang']))
+			{
+				$this->setHeaderLang($this->settings['header_builder']['accept_lang']);
+			}
+
+			if(isset($this->settings['header_builder']['content_type']))
+			{
+				$this->setHeaderContentType($this->settings['header_builder']['content_type']);
+			}
+
+			if(isset($this->settings['header_builder']['connection']))
+			{
+				$this->setHeaderConnection($this->settings['header_builder']['connection']);
+			}
+
+			if(isset($this->settings['header_builder']['encoding']))
+			{
+				$this->setHeaderEncode($this->settings['header_builder']['encoding']);
+			}
+
+			if(isset($this->settings['header_builder']['extra_header']))
+			{
+				foreach ($this->settings['header_builder']['extra_header'] as $key => $value) 
+				{
+					$this->setHeaderExtraHeader($key, $value);
+				}
+			}
+
+			$this->buildHeader();
+		}
+
+		if(isset($this->settings['nobody']))
+		{
+			$this->setNobody($this->settings['nobody']);
+		}
+
+		if(isset($this->settings['timeout']))
+		{
+			$this->setCTO($this->settings['timeout']);
+		}
+
+		if(isset($this->settings['verify_host']))
+		{
+			$this->setVerifyHost($this->settings['verify_host']);
+		}
+
+		if(isset($this->settings['verify_peer']))
+		{
+			$this->setVerifyPeer($this->settings['verify_peer']);
+		}
+
+		if(isset($this->settings['post']))
+		{
+			$this->setPost($this->settings['post']);
+		}
+
+		if(isset($this->settings['custom_req']['type']))
+		{
+			if(isset($this->settings['custom_req']['data']))
+			{
+				$data = $this->settings['custom_req']['data'];
+			}
+			else
+			{
+				$data = null;
+			}
+			$this->setCustomReq($this->settings['custom_req']['type'], $data);
+		}
+
+		$this->buildOpt();
+		$response = $this->exec();
+
+		if(isset($this->settings['body_only']) && $this->settings['body_only'] = true)
+		{
+			$response = $this->getBodyResponse();
+		}
+
+		if(isset($this->settings['header_only']) && $this->settings['header_only'] = true)
+		{
+			$response = $this->getHeaderResponse();
+		}
+
+		return $response;
 	}
 }
 
